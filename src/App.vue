1146f966cf94ef95af20a325b094cd50434f873d
Jokerzc <44741907+Jokerzc@users.noreply.github.com>
2024-01-26 14:03:07 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView /> -->
+  <a-layout>
+    <a-layout-sider v-model:collapsed="collapsed" collapsible>
+      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>
+            <RouterLink to="/">主页面</RouterLink>
+          </span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>
+            <RouterLink to="/TwoPage">TwoPage</RouterLink>
+          </span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout style="min-height: 100vh;">
+      <RouterView />
+    </a-layout>
+  </a-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons-vue';
+const selectedKeys = ref(['1']);
+const collapsed = ref(false);
+
+</script>
+
+<style lang="less">
+#app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.3);
+  margin: 16px;
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="main-page">
+    <div ref="mpBox" class="mpBox">
+      <img ref="mpImg" src="../assets/images/Page1/人1.png" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const mpBox = ref(null)
+const mpImg = ref(null)
+
+function computedTransform(num, doc) {
+  return (num / doc * 40 - 20).toFixed(1)
+}
+
+function computedBGPosition(num, doc) {
+  return (60 - Number((num / doc * 20).toFixed(1)) + '%')
+}
+
+onMounted(() => {
+  document.addEventListener('mousemove', e => {
+    mpBox.value.style.transform = `
+      rotateX(${computedTransform(e.clientX, window.innerWidth)}deg)
+      rotateY(${computedTransform(e.clientY, window.innerHeight)}deg)
+    `
+    mpBox.value.style.backgroundPosition = `
+      ${computedBGPosition(e.clientX, window.innerWidth)} 
+      ${computedBGPosition(e.clientY, window.innerHeight)}
+    `
+    mpImg.value.style.transform = `
+      translateX(${computedTransform(e.clientX, window.innerWidth)}px)
+      translateY(${computedTransform(e.clientY, window.innerHeight)}px)
+    `
+  })
+})
+
+</script>
+
+<style>
+.main-page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .mpBox {
+    width: 175px;
+    height: 250px;
+    background: #fff;
+    border-radius: 8px;
+    box-shadow: 0px 10px 20px 20px rgba(0, 0, 0, 0.17);
+    position: absolute;
+    font-size: 42px;
+    color: #000;
+    text-align: center;
+    background-image: url('../assets/images/Page1/背景3.jpg');
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    background-size: 120% 120%;
+    overflow: hidden;
+
+    img {
+      height: 100%;
+      position: relative;
+      top: 25px;
+      left: 10px;
+    }
+  }
+}
+</style>
